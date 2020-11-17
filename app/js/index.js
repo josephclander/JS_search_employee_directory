1d9501140f51386a525aeb3d5e93b5a0b8936a05
Joseph Lander <josephlander@pjs-macbook-pro.lan>
2020-11-17 11:36:51 +0000

    Add httpRequest succesful log to console

---
+++ app/js/index.js
@@ -0,0 +1,24 @@
+// This method taught in the Treehouse was implemented successfully
+
+// var xhr = new XMLHttpRequest();
+// xhr.onreadystatechange = function () {
+//   if (xhr.readyState === 4) {
+//     const { results } = JSON.parse(xhr.responseText);
+//     console.log(results);
+//   }
+// };
+// xhr.open(
+//   'GET',
+//   'https://randomuser.me/api/?results=12&inc=name,email,location,cell,dob,picture&nat=gb'
+// );
+// xhr.send();
+
+// Moving forward the fetch api has been used
+
+fetch(
+  'https://randomuser.me/api/?results=12&inc=name,email,location,cell,dob,picture&nat=gb'
+)
+  .then((response) => response.json())
+  .then(({ results }) => {
+    console.log(results);
+  });
